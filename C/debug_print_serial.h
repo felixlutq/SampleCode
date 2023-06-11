9489cc703dcebb728f21a46d6e0b69bd0c0bae0e
Felix <101188365+felixlutq@users.noreply.github.com>
2023-06-11 18:49:36 +1000

    Debug print to serial

---
+++ C/debug_print_serial.h
@@ -0,0 +1,14 @@
+#ifdef DEBUG
+void debug_print(const char *format, ...)
+{
+   char strDest[256] = {0};
+   va_list args;
+   va_start( args, format );
+   vsprintf( strDest, format, args );
+   va_end( args );
+   Serial.println(strDest);
+}
+#define DEBUG_PRINT debug_print
+#else
+    # define DEBUG_PRINT(...)
+#endif
